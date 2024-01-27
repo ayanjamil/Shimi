1e0170314ef6f6d4981db3ccf41e7270a6a2b774
ayanjamil <ayanjamil00@gmail.com>
2024-01-27 17:32:56 +0530

    making onboarding screen functional comonent

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -10,113 +10,112 @@ import {
 import { Block, Button, Text, theme } from "galio-framework";
 const { height, width } = Dimensions.get("screen");
 import Images from "../constants/Images";
+import { useNavigation } from "@react-navigation/native";
 
-class Onboarding extends React.Component {
-  render() {
-    const { navigation } = this.props;
-    return (
-      <Block flex style={styles.container}>
-        <Block flex center>
-          <ImageBackground
-            source={Images.Onboarding}
-            style={{ height, width, zIndex: 1, opacity: 0.25 }}
-          />
-        </Block>
-        <Block flex style={{ flex: 6 }}>
-          <TouchableOpacity onPress={() => navigation.navigate("App")}>
-            <Block center>
-              <Image source={Images.LogoOnboarding} style={styles.logo} />
-            </Block>
-          </TouchableOpacity>
-
-          <Block style={{ marginVertical: 50 }}>
-            <Text
-              center
-              bold
-              color="black"
-              size={45}
-              style={{
-                marginVertical: 10,
-                textShadowColor: "rgba(0, 0, 0, 0.20)",
-                textShadowOffset: { width: -1, height: 4 },
-                textShadowRadius: 5,
-              }}
-            >
-              {"Your Fashion Moodboard "}
-            </Text>
-            <Text
-              center
-              bold
-              size={18}
-              style={{
-                color: "#6B6B6B",
-                textShadowColor: "rgba(0, 0, 0, 0.20)",
-                textShadowOffset: { width: -1, height: 4 },
-                textShadowRadius: 5,
-              }}
-            >
-              {"Be Inspired"}
-            </Text>
+const Onboardinig = (props) => {
+  const navigation = useNavigation();
+  return (
+    <Block flex style={styles.container}>
+      <Block flex center>
+        <ImageBackground
+          source={Images.Onboarding}
+          style={{ height, width, zIndex: 1, opacity: 0.25 }}
+        />
+      </Block>
+      <Block flex style={{ flex: 6 }}>
+        <TouchableOpacity onPress={() => navigation.navigate("App")}>
+          <Block center>
+            <Image source={Images.LogoOnboarding} style={styles.logo} />
           </Block>
-          <Block
-            flex
+        </TouchableOpacity>
+
+        <Block style={{ marginVertical: 50 }}>
+          <Text
+            center
+            bold
+            color="black"
+            size={45}
             style={{
-              marginBottom: 60,
-              flex: 4.5,
-              marginHorizontal: 30,
-              marginBottom: 60,
+              marginVertical: 10,
+              textShadowColor: "rgba(0, 0, 0, 0.20)",
+              textShadowOffset: { width: -1, height: 4 },
+              textShadowRadius: 5,
             }}
           >
-            <TouchableOpacity
-              style={{
-                backgroundColor: "#000",
-                paddingVertical: 10,
-                marginVertical: 15,
-                paddingHorizontal: 20,
-                borderRadius: 25,
-              }}
-              // onPress={() => console.log("Sign Up Pressed")}
-            >
-              <Text center bold style={styles.text}>
-                Sign Up
-              </Text>
-            </TouchableOpacity>
+            {"Your Fashion Moodboard "}
+          </Text>
+          <Text
+            center
+            bold
+            size={18}
+            style={{
+              color: "#6B6B6B",
+              textShadowColor: "rgba(0, 0, 0, 0.20)",
+              textShadowOffset: { width: -1, height: 4 },
+              textShadowRadius: 5,
+            }}
+          >
+            {"Be Inspired"}
+          </Text>
+        </Block>
+        <Block
+          flex
+          style={{
+            marginBottom: 60,
+            flex: 4.5,
+            marginHorizontal: 30,
+            marginBottom: 60,
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#000",
+              paddingVertical: 10,
+              marginVertical: 15,
+              paddingHorizontal: 20,
+              borderRadius: 25,
+            }}
+            onPress={() => console.log("Sign Up Pressed")}
+          >
+            <Text center bold style={styles.text}>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              style={{
-                backgroundColor: "#5190E8",
-                paddingVertical: 10,
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#5190E8",
+              paddingVertical: 10,
 
-                borderRadius: 25,
+              borderRadius: 25,
+            }}
+            onPress={() => console.log("Continue with Google Pressed")}
+          >
+            <Text center bold style={styles.text}>
+              Continue with Google
+            </Text>
+          </TouchableOpacity>
+        </Block>
+        <Block flex center style={{ flexDirection: "row" }}>
+          <Text style={{ fontSize: 16 }}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => console.log("Button Pressed")}>
+            <Text
+              center
+              style={{
+                color: "blue",
+                alignSelf: "center",
+                marginHorizontal: 5,
+                fontSize: 16,
               }}
-              onPress={() => console.log("Continue with Google Pressed")}
             >
-              <Text center bold style={styles.text}>
-                Continue with Google
-              </Text>
-            </TouchableOpacity>
-          </Block>
-          <Block flex center style={{ flexDirection: "row" }}>
-            <Text style={{ fontSize: 16 }}>Already have an account?</Text>
-            <TouchableOpacity onPress={() => console.log("Button Pressed")}>
-              <Text
-                center
-                style={{
-                  color: "blue",
-                  alignSelf: "center",
-                  marginHorizontal: 5,
-                  fontSize: 16,
-                }}
-              >
-                Login
-              </Text>
-            </TouchableOpacity>
-          </Block>
+              Login
+            </Text>
+          </TouchableOpacity>
         </Block>
       </Block>
-    );
-  }
-}
+    </Block>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -137,4 +136,4 @@ const styles = StyleSheet.create({
   loginText: {},
 });
 
-export default Onboarding;
+export default Onboardinig;
