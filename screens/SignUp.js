b6f047d739afcb343c6c35ad1c57f7708b7fea54
ayanjamil <ayanjamil00@gmail.com>
2024-02-04 20:03:35 +0530

    Creating a development build

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -14,8 +14,29 @@ const { height, width } = Dimensions.get("screen");
 import { useNavigation } from "@react-navigation/native";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../config/firebase";
+// import { auth, db } from "../config/firebase";
 import { collection, addDoc } from "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+const firebaseConfig = {
+  apiKey: "AIzaSyCrcLMbeWfPvHwAfGMtNDM8NonUH5l4yGY",
+  authDomain: "shimi-1c1e8.firebaseapp.com",
+  projectId: "shimi-1c1e8",
+  storageBucket: "shimi-1c1e8.appspot.com",
+  messagingSenderId: "904122348831",
+  appId: "1:904122348831:web:215dc6b0f0a5104a002c2a",
+  measurementId: "G-C56NV5V0ZZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+// export { app };
+// export const db = getFirestore(initializeApp(firebaseConfig));
 
 const SignUp = (props) => {
   const navigation = useNavigation();
